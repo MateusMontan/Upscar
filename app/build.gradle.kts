6c18ca7485f09cdb99b257da9bb839b3812bd2e1
Mateus Montan <mateusinfocefetmg@gmail.com>
2024-09-23 21:04:35 -0300

    Adicionando tela

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
